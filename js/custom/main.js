d422931853eb2616706da532b50cf7af95a8f208
@sebashornan <shornan@unprg.edu.pe>
2023-06-25 20:47:06 -0500

    Cambios

--- js/custom/main.js
+++ js/custom/main.js
@@ -27,6 +27,27 @@ function validarDatos() {
             alert("Su correo electrónico está mal");
             return false;
         }
+        let inputPassword = document.getElementById("password").value;
+        /* Referencia al valor del elemento #user */
+        let inputRePassword = document.getElementById("repassword").value;
+
+        if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/.test(inputPassword) == false) {
+            document.getElementById("password").value = "";
+            document.getElementById("repassword").value = "";
+            alert("La contraseña NO CUMPLE con los requisitos mínimos.");
+            return false;
+        }
+
+        /* Validar si las contraseñas no son iguales */
+        if (inputPassword != inputRePassword) {
+            /* Referencia al elemento de apoyo */
+            let apoyoInputPassword = document.getElementById("apoyoInputPassword");
+            /* Cambiar el atributo style */
+            apoyoInputPassword.setAttribute("style", "");
+            document.getElementById("repassword").value = "";
+            alert("Las contraseñas no son iguales.");
+            return false;
+        }
 
         /* Mandar confirmación de envio */
         return true;
